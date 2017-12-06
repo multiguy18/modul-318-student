bf2a537e4971dfeacee683290e48a28f42c5e8e4
Anselm Schacher <anselm.schacher@kauz.ch>
2017-12-06 13:59:00 +0100

    Added WinForms project

---
+++ PublicNavWinForms/PublicNavForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PublicNavWinForms
+{
+    public partial class PublicNavForm : Form
+    {
+        public PublicNavForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
